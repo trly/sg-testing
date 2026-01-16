6ad6de82a5ad922132e29ede30f3aaa0d4cd2fdc
Travis Lyons <travis.lyons@gmail.com>
2026-01-16 11:44:31 -0500

    reset repo

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>CVE-2025-55182 Vulnerable Test App</h1>
-      <p>This app uses vulnerable versions of Next.js and React for security scanner testing.</p>
-    </main>
-  )
-}
