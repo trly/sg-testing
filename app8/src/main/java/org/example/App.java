4832a7520d0463ffc05e6035e5677ebc379e94ba
Travis Lyons <travis.lyons@gmail.com>
2026-02-02 14:40:41 -0500

    multiple spring-boot apps for testing search on yaml properties

---
+++ app8/src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Hello from App8 on port 8087!";
+    }
+}
